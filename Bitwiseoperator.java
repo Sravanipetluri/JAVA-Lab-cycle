e59d6942d92a54d736776dea4031e5c670b9a633
Sravanipetluri <79572520+Sravanipetluri@users.noreply.github.com>
2021-09-21 12:46:21 +0530

    Update Bitwiseoperator.java

--- Bitwiseoperator.java
+++ Bitwiseoperator.java
@@ -41,3 +41,65 @@ class Bitwiseoperator
   }while(ch<=7);
  }
 }
+
+
+output:
+enter a value5
+enter b value6
+1.bitwise AND(&)
+2.bitwise OR(|)
+3.bitwise EXCLUSIVE OR(^)
+4.bitwise NEGATION(~)
+5.bitwise LEFTSHIFT(<<)
+6.bitwise RIGHTSHIFT(>>)
+7.bitwise ZEROFILLWITH RIGHTSHIFT(>>>)
+enter choice
+1
+a&b=4
+1.bitwise AND(&)
+2.bitwise OR(|)
+3.bitwise EXCLUSIVE OR(^)
+4.bitwise NEGATION(~)
+5.bitwise LEFTSHIFT(<<)
+6.bitwise RIGHTSHIFT(>>)
+7.bitwise ZEROFILLWITH RIGHTSHIFT(>>>)
+enter choice
+5
+a<<b=320
+1.bitwise AND(&)
+2.bitwise OR(|)
+3.bitwise EXCLUSIVE OR(^)
+4.bitwise NEGATION(~)
+5.bitwise LEFTSHIFT(<<)
+6.bitwise RIGHTSHIFT(>>)
+7.bitwise ZEROFILLWITH RIGHTSHIFT(>>>)
+enter choice
+4
+~a=-6
+1.bitwise AND(&)
+2.bitwise OR(|)
+3.bitwise EXCLUSIVE OR(^)
+4.bitwise NEGATION(~)
+5.bitwise LEFTSHIFT(<<)
+6.bitwise RIGHTSHIFT(>>)
+7.bitwise ZEROFILLWITH RIGHTSHIFT(>>>)
+enter choice
+2
+a|b=7
+1.bitwise AND(&)
+2.bitwise OR(|)
+3.bitwise EXCLUSIVE OR(^)
+4.bitwise NEGATION(~)
+5.bitwise LEFTSHIFT(<<)
+6.bitwise RIGHTSHIFT(>>)
+7.bitwise ZEROFILLWITH RIGHTSHIFT(>>>)
+enter choice
+3
+a^b=3
+1.bitwise AND(&)
+2.bitwise OR(|)
+3.bitwise EXCLUSIVE OR(^)
+4.bitwise NEGATION(~)
+5.bitwise LEFTSHIFT(<<)
+6.bitwise RIGHTSHIFT(>>)
+7.bitwise ZEROFILLWITH RIGHTSHIFT(>>>)
