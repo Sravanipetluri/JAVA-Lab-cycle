0c28b687e2b43b9998669b185728c14f80c9a324
Sravanipetluri <79572520+Sravanipetluri@users.noreply.github.com>
2021-10-01 22:15:34 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Area
+{
+void area(float a,float b)
+{
+float c=a*b;
+System.out.println("area of rectangle: "+c);
+}
+void area(float a)
+{
+float d=a*a;
+System.out.println("area of square :" +d);
+}
+void area(double x,double y)
+{
+double e =0.5*x*y;
+System.out.println("area of triangle :" +e);
+}
+public static void main(String[] args)
+{
+Area ob=new Area ();
+ob.area(1,2);
+ob.area(3);
+ob.area(2.0,4.0);
+}
+}
+
+output:
+area of rectangle: 2.0
+area of square :9.0
+area of triangle :4.0
