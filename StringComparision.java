4b26af11198a93766e30d95473ad86d3729c50b1
Sravanipetluri <79572520+Sravanipetluri@users.noreply.github.com>
2021-10-01 22:10:59 +0530

    Add files via upload

---
+++ StringComparision.java
@@ -0,0 +1,34 @@
+//Comparison of two strings
+
+public class String_compare { 
+	public static void main(String args[]) 
+	{ 
+		String string1 = new String("python"); 
+		String string2 = new String("python"); 
+		String string3 = new String("C"); 
+		String string4 = new String("Ruby"); 
+		String string5 = new String("SQL"); 
+
+		
+		System.out.println("Comparing " + string1 + " and " + string2 
+						+ " : " + string1.equals(string2)); 
+
+		
+		System.out.println("Comparing " + string3 + " and " + string4 
+						+ " : " + string3.equals(string4)); 
+
+		
+		System.out.println("Comparing " + string4 + " and " + string5 
+						+ " : " + string4.equals(string5)); 
+
+		 
+		System.out.println("Comparing " + string1 + " and " + string4 
+						+ " : " + string1.equals(string4)); 
+	} 
+} 
+
+output:
+Comparing python and python : true
+Comparing C and Ruby : false
+Comparing Ruby and SQL : false
+Comparing python and Ruby : false
